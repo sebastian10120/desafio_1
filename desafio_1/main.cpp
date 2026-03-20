beca03d3a5d8db5286c2a6c048872b996f90454c
sebastian cano sanchez <sebastian.cano5@udea.edu.co>
2026-03-20 10:34:33 -0500

    se añadio la condicion de derrota, junto al borrado de una linea entera

--- desafio_1/main.cpp
+++ desafio_1/main.cpp
@@ -12,17 +12,21 @@ int **inicial_pieza(int &fila,int &columna);
 bool juego(int **matriz,int ancho, int alto);
 char movimiento_de_ficha();
 void borrar_matriz(int **matriz, int largo);
-bool colision();
 void limpiar_matriz(int **matriz, int alto, int ancho);
 void limpiar_movimiento( int **matriz,int **pieza, int alto, int ancho, int alto_actual, int ancho_actual);
+void imprimir_movimiento(int **matriz,int **pieza, int alto, int ancho, int alto_actual, int ancho_actual);
+bool colision(int **matriz,int **pieza, int alto, int ancho, int alto_actual, int ancho_actual, int alto_matriz, int ancho_matriz, char movimiento);
+bool condicion_derrota(int **matriz, int altura_matriz, int ancho_matriz);
+void eliminacion_de_linea(int **matriz, int altura_matriz, int ancho_matriz);
 /*prototipado*/
 int main()
 {
     unsigned short int ancho, numero_pieza;
+    const unsigned short  int alto = 16;
     char dato_entrada_menu,tamaño_matriz;
     bool juego_iniciar(true), empezar_partida;
     do{
-        cout << "elija una de las opciones para continuar\n1) iniciar juego\n2) salir\n";
+        cout << "elija una de las opciones para continuar \n1) iniciar juego \n2) salir\n";
         dato_entrada_menu = confirmacion_de_menu();
         if (dato_entrada_menu == '1'){
             bool seleccion_tamaño(true);
@@ -32,19 +36,19 @@ int main()
             tamaño_matriz = seleccion_tamaño_de_la_matriz();
                 if (tamaño_matriz == '1'){
                     ancho = 8;
-                    matriz = crear_matriz(ancho,(ancho+4));
+                    matriz = crear_matriz(ancho,alto);
                     empezar_partida = true;
                     seleccion_tamaño = false;
                 }
                 else if (tamaño_matriz == '2'){
                     ancho = 16;
-                    matriz = crear_matriz(ancho,(ancho+4));
+                    matriz = crear_matriz(ancho,alto);
                     empezar_partida = true;
                     seleccion_tamaño = false;
                 }
                 else if (tamaño_matriz == '3'){
                     ancho = 24;
-                    matriz = crear_matriz(ancho,(ancho+4));
+                    matriz = crear_matriz(ancho,alto);
                     empezar_partida = true;
                     seleccion_tamaño = false;
                 }
@@ -55,8 +59,8 @@ int main()
             }
             while(seleccion_tamaño == true);
             while (empezar_partida == true){
-                empezar_partida=juego(matriz,ancho,ancho+4);
-                borrar_matriz(matriz,ancho+4);
+                empezar_partida=juego(matriz,ancho,alto);
+                borrar_matriz(matriz,alto);
             }
         }
         else if (dato_entrada_menu == '2'){
@@ -85,7 +89,7 @@ char seleccion_tamaño_de_la_matriz(){
     char resultado;
     bool dato_recolectado(false);
     char datos_posibles[4] = {'1','2','3','q'},dato[150];
-    cout << "seleccione el tamaño del campo de juego\n1) 12x8\n2) 12x16\n3) 12x32\nq) salir\naclaracion el primer numero es el alto el segundo el ancho";
+    cout << "seleccione el tamaño del campo de juego\n1) 16x8\n2) 16x16\n3) 16x24\nq) salir\naclaracion el primer numero es el alto el segundo el ancho";
     cin >> dato;
     for (int i =0; i < (sizeof(dato)/sizeof(dato[0])) && dato_recolectado == false;i++){
         dato[i] = letra_minuscula(dato[i]);
@@ -109,18 +113,16 @@ void imprimir_matriz(int **matriz, int ancho, int largo){
     for (int i = 0; i < largo; i++){
         for (int j = 0; j < ancho; j++){
             if (i > 3){
-                // Filas normales: si el bit no coincide con 0, imprimimos bloque
                 if ((mask) & matriz[i][j]){
-                    cout << "0";
+                    cout << " 0 ";
                 } else {
-                    cout << "#";
+                    cout << " # ";
                 }
             } else {
-                // Primeras 4 filas: imprimimos solo si el bit está activo
                 if ((mask) & matriz[i][j]){
-                    cout << "0";
+                    cout << " 0 ";
                 } else {
-                    cout << " ";
+                    cout << "   ";
                 }
             }
         }
@@ -142,55 +144,74 @@ int numero_ramdom(){
 }
 bool juego(int **matriz,int ancho, int alto){
     bool juego(true);
-    int pieza_fila;
-    int pieza_columna;
-    char movimiento;
-    int **pieza_en_juego = inicial_pieza(pieza_fila,pieza_columna);
-    int columna_actual = (ancho-pieza_columna)/2;
-    int fila_actual(0);
     do{
-        for(int i = 0; i < pieza_fila;i++){
-            for(int j =0; j < pieza_columna; j++){
-                if( pieza_en_juego[i][j] == 1){
-                    matriz[i+fila_actual][j+columna_actual] = 1;
+        bool cambio_pieza = true;
+        int pieza_fila, pieza_columna;
+        int **pieza_en_juego = inicial_pieza(pieza_fila,pieza_columna);
+        int columna_actual = (ancho - pieza_columna) / 2;
+        int fila_actual = 0;
+        bool pieza_activa = true;
+        char movimiento;
+        while(cambio_pieza == true){
+            imprimir_movimiento(matriz,pieza_en_juego,pieza_fila,pieza_columna,fila_actual,columna_actual);
+            imprimir_matriz(matriz, ancho, alto);
+            cout << "movimientos\n (a)izq (s)abj (d)der (w)girar (q)salir \n";
+            movimiento = movimiento_de_ficha();
+            if (movimiento == 'a'){
+                if (columna_actual>0){
+                    limpiar_movimiento(matriz,pieza_en_juego,pieza_fila,pieza_columna,fila_actual,columna_actual);
+                    columna_actual--;
+                    if(colision(matriz,pieza_en_juego,pieza_fila,pieza_columna,fila_actual,columna_actual, alto, ancho, movimiento) == false){
+                        columna_actual++;
+                        imprimir_movimiento(matriz,pieza_en_juego,pieza_fila,pieza_columna,fila_actual,columna_actual);
+                    }
+                    if (columna_actual < 0){
+                        columna_actual++;
+                    }
                 }
             }
-        }
-        imprimir_matriz(matriz, ancho, alto);
-        cout << "movimientos\n (a)izq (s)abj (d)der (w)girar (q)salir \n";
-        movimiento = movimiento_de_ficha();
-        if (movimiento == 'a'){
-            if (columna_actual>0){
-                limpiar_movimiento(matriz,pieza_en_juego,pieza_fila,pieza_columna,fila_actual,columna_actual);
-                columna_actual--;
-            }
-        }
-        else if (movimiento == 's'){
-            if (fila_actual+pieza_fila<alto){
-                limpiar_movimiento(matriz,pieza_en_juego,pieza_fila,pieza_columna,fila_actual,columna_actual);
-                fila_actual++;
-            }
-        }
-        else if (movimiento == 'd'){
-            if (columna_actual+pieza_columna<ancho){
-                limpiar_movimiento(matriz,pieza_en_juego,pieza_fila,pieza_columna,fila_actual,columna_actual);
-                if (){
-
+            else if (movimiento == 's'){
+                if (fila_actual+pieza_fila<=alto){
+                    limpiar_movimiento(matriz,pieza_en_juego,pieza_fila,pieza_columna,fila_actual,columna_actual);
+                    fila_actual++;
+                    if(colision(matriz,pieza_en_juego,pieza_fila,pieza_columna,fila_actual,columna_actual, alto, ancho, movimiento) == false){
+                        fila_actual--;
+                        imprimir_movimiento(matriz,pieza_en_juego,pieza_fila,pieza_columna,fila_actual,columna_actual);
+                        cambio_pieza = false;
+                    }
                 }
-                columna_actual++;
+            }
+            else if (movimiento == 'd'){
+                if (columna_actual+pieza_columna<ancho){
+                    limpiar_movimiento(matriz,pieza_en_juego,pieza_fila,pieza_columna,fila_actual,columna_actual);
+                    columna_actual++;
+                    if(colision(matriz,pieza_en_juego,pieza_fila,pieza_columna,fila_actual,columna_actual, alto, ancho, movimiento) == false){
+                        columna_actual--;
+                        imprimir_movimiento(matriz,pieza_en_juego,pieza_fila,pieza_columna,fila_actual,columna_actual);
+                    }
+                    if((columna_actual+pieza_columna > ancho)){
+                        columna_actual--;
+                    }
+                }
+            }
+            else if (movimiento == 'w'){
+                cout << "nada por el momento";
+                limpiar_movimiento(matriz,pieza_en_juego,pieza_fila,pieza_columna,fila_actual,columna_actual);
+            }
+            else if (movimiento == 'q'){
+                juego = false;
+                cambio_pieza=false;
             }
         }
-        else if (movimiento == 'w'){
-            cout << "nada por el momento";
-            limpiar_movimiento(matriz,pieza_en_juego,pieza_fila,pieza_columna,fila_actual,columna_actual);
-        }
-        else if (movimiento == 'q'){
+        borrar_matriz(pieza_en_juego,pieza_fila);
+        eliminacion_de_linea(matriz,alto,ancho);
+        if (condicion_derrota(matriz, alto, ancho) == true){
             juego = false;
+            cout << "PERDISTE\n\n\n";
         }
-
     }
     while(juego == true);
-    borrar_matriz(pieza_en_juego,pieza_fila);
+
     return juego;
 }
 int **inicial_pieza(int &fila,int &columna){
@@ -293,9 +314,31 @@ char movimiento_de_ficha(){
     cin.ignore(150, '\n');
     return devolucion;
 }
-bool colision(){
-    bool colisor;
-    return colisor;
+bool colision(int **matriz,int **pieza, int alto, int ancho, int alto_actual, int ancho_actual, int alto_matriz, int ancho_matriz, char movimiento){
+    for(int i = 0; i < alto;i++){
+        for(int j =0; j < ancho; j++){
+            if( pieza[i][j] == 1){
+                if(movimiento == 's'){
+                    if( i+alto_actual == alto_matriz|| matriz[alto_actual+i][ancho_actual+j] == 1){
+                        return false;
+                    }
+                }
+
+                if(movimiento == 'a'){
+                    if(matriz[alto_actual+i][ancho_actual+j] == 1){
+                        return false;
+                    }
+                }
+
+                if(movimiento == 'd'){
+                    if( matriz[alto_actual+i][ancho_actual+j] == 1){
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    return true;
 }
 void borrar_matriz(int **matriz, int largo){
     for(int i = 0; i < largo; i++){
@@ -310,5 +353,55 @@ void limpiar_movimiento( int **matriz,int **pieza, int alto, int ancho, int alto
                 matriz[i+alto_actual][j+ancho_actual] = 0;
             }
         }
+    }
+}
+void imprimir_movimiento(int **matriz,int **pieza, int alto, int ancho, int alto_actual, int ancho_actual){
+    for(int i = 0; i < alto;i++){
+        for(int j =0; j < ancho; j++){
+            if( pieza[i][j] == 1){
+                matriz[i+alto_actual][j+ancho_actual] = 1;
+            }
+        }
+    }
+}
+void eliminacion_de_linea(int **matriz, int altura_matriz, int ancho_matriz){
+    int mask = 1;
+    for (int i = altura_matriz-1; i >= 0;i--){
+        int  contador = 0;
+        for (int j = ancho_matriz-1; j >= 0;j--){
+            if (mask & matriz[i][j]){
+                contador++;
+            }
+        }
+        if (contador == ancho_matriz){
+            for(int filas_arriba = i; filas_arriba>0;filas_arriba--){
+                for(int j = 0; j < ancho_matriz; j++){
+                    matriz [filas_arriba][j] = matriz[filas_arriba-1][j];
+                }
+            }
+            i++;
+            for(int j = 0; j < ancho_matriz; j++){
+                matriz[0][j] = 0;
+            }
+        }
+
+    }
+}
+
+bool condicion_derrota(int **matriz, int altura_matriz, int ancho_matriz){
+    int contador = 0, mask = 1;
+    for(int fila_vacia = 0; fila_vacia <= 3 && contador == 0;fila_vacia++){
+        for (int j = ancho_matriz-1; j >= 0;j--){
+            if (mask & matriz[fila_vacia][j]){
+                contador++;
+                break;
+            }
+        }
+    }
+    if(contador > 0){
+        return true;
+    }
+    else{
+        return false;
     }
 }
